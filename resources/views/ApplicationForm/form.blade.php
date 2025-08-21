e78fa3289e6339151b674ac6d23a8ded93bb12f8
root <dillon.raymond@gov.tt>
2025-08-21 13:58:07 -0400

    Updated wording and background colour

--- resources/views/ApplicationForm/form.blade.php
+++ resources/views/ApplicationForm/form.blade.php
@@ -3,30 +3,30 @@
 @section('Content')
 
 <body>
-    <div id="application-form" class="container-fluid">
+    <div id="application-form" class="container-fluid" style="background-color: #d8f3dc">
         {{-- <img src="https://apps.mydns.gov.tt/images/rapp_participant_banner.jpg" alt="Logo" class="img-fluid w-100"> --}}
         <section>
             <div class="text-center">
                 <h1>GERIATRIC ADOLESCENT PARTNERSHIP PROGRAMME</h1>
-                <h3>G.A.P.P.</h3>
+                <h2>GAPP</h2>
             </div><br>
-            <p>A call for application for caregivers to join the Geriatric Adolescent Partnership Programme (GAPP). Requirements:
+            <p><strong>A call for application for Caregivers to join the Geriatric Adolescent Partnership Programme (GAPP) Requirements:</strong>
             <ul>
                 <li>18-35 years</li>
-                <li>Possess minimum of two (2) CSEC passes</li>
+                <li>Possess minimum of two (2) CXC/CSEC passes</li>
                 <li>National of Trinidad and Tobago</li>
                 <li>Certificate in Geriatric Care or Professional Healthcare</li>
                 <li>The applicant must be available to provide service/care for a period of 6 months (following successful completion of training).</li>
             </ul>
 
-            Applicants must submit the following documents when applying:
+            <strong>Applicants must submit the following documents when applying:</strong>
             <ul>
-                <li>Copy of Academic and/or skills training Certificates</li>
+                <li>Copy of Academic and/or Skills Training Certificates</li>
                 <li>Copy of National Identification (ID Card, Passport, Birth Certificate)</li>
                 <li>Certificate of Character or Receipt from the TTPS</li>
-                <li>(Two) 2 Letters of Recommendation</li>
+                <li>Two (2) Letters of Recommendation</li>
                 <li>Copy of National Insurance card (optional)</li>
-                <li>Proof of Address - Utility Bill <strong>OR</strong> Top section of Bank Statement (If the bill is not in your name, provide a letter of authorisation from the homeowner, plus a copy of their ID, Passport or Driver's Permit. Accepted utility bills include Cable, Electricity, Water, Phone (landlines), and on premise Internet.)</li>
+                <li>Proof of Address - Utility Bill <strong>OR</strong> Top section of Bank Statement (If the bill is not in your name, provide a Letter of Authorisation from the homeowner, plus a copy of their ID, Passport or Driver's Permit. Accepted utility bills include Cable, Electricity, Water, Phone (landlines), and on premise Internet.)</li>
             </ul>
 
             You must complete the entire form for your application to be eligible (or considered) for selection.
@@ -54,8 +54,8 @@
         <section>
             @php
             $yesNoOptions = [
-            ['Yes','Y'],
-            ['No','N'],
+                ['Yes','Y'],
+                ['No','N'],
             ];
             // Reset question counter for this form
             \App\View\Components\Form\QuestionNumbering::reset();
@@ -105,7 +105,7 @@
                         ['Sangre Grande/St Andrew', 'Sangre Grande/St Andrew'],
                         ['Tobago', 'Tobago'],
                         ['Toco/St David', 'Toco/St David'],
-                        ['Tunapuna/Piarco', 'Tunapuna/Piarco'],
+                        ['Arouca/Tunapuna/Piarco', 'Arouca/Tunapuna/Piarco'],
                     ]" />
 
                 {{-- 4. GENDER --}}
@@ -155,14 +155,14 @@
 
                 {{-- 8. Employment Status --}}
                 <div x-data="{ APL_Employment_Status: '{{ old('APL_Employment_Status', '') }}' }">
-                    <x-form.radio id="APL_Employment_Status" label="Are you employed/self-employed?" :options="$yesNoOptions" x-model="APL_Employment_Status" />
+                    <x-form.radio id="APL_Employment_Status" label="Are you Employed/Self-Employed?" :options="$yesNoOptions" x-model="APL_Employment_Status" />
 
                     <div x-show="APL_Employment_Status == 'Y'" x-cloak class="mt-4">
                         <x-form.text-input id="APL_Job_Title" name="APL_Job_Title" label="Job Title (if applicable)" :required="false" :questionNumber="false" />
                         <x-form.radio id="APL_Employment_Type" label="Employment Type" :options="[
-                                ['Full-time', 'Full-time'],
-                                ['Part-time', 'Part-time'],
-                                ['Self-employed (Business owner)', 'Self-employed'],
+                                ['Full-Time', 'Full-Time'],
+                                ['Part-Time', 'Part-Time'],
+                                ['Self-Employed (Business Owner)', 'Self-Employed'],
                                 ['Both Employed and Business Owner', 'Both'],
                             ]" :required="false" :questionNumber="false" />
                     </div>
@@ -178,7 +178,7 @@
                 <x-form.text-input id="APL_Email" label="Email Address" />
 
                 {{-- 12. IDENTIFICATION --}}
-                <x-form.select id="APL_ID_TYP" label="Please provide the number for one of the following forms of identification" :options="[
+                <x-form.select id="APL_ID_TYP" label="Please provide the number for one of the following forms of identification." :options="[
                         ['National Identification Card', 'NID'],
                         ['Passport', 'PP'],
                     ]" />
@@ -198,27 +198,27 @@
                 </div>
 
                 {{-- 15. DO YOU HAVE A BANK ACCOUNT? --}}
-                <x-form.radio id="APL_Has_Bank_Account" label="Do you have a bank account?" :options="$yesNoOptions" />
+                <x-form.radio id="APL_Has_Bank_Account" label="Do you have a Bank Account?" :options="$yesNoOptions" />
 
                 {{-- 16. WHAT IS YOUR PREFERRED REGION FOR PLACEMENT? --}}
                 <x-form.select id="APL_Service_Area" label="What is your preferred region for placement?" :options="[
                         ['Northern Region (Curepe to Carenage)', 'Northern Region (Curepe to Carenage)'],
                         ['St Patrick Region (Icacos to Penal)', 'St Patrick Region (Icacos to Penal)'],
-                        ['Victoria (San Fernando to Princess Town)', 'Victoria (San Fernando to Princess Town)'],
+                        ['Victoria (San Fernando to Princes Town)', 'Victoria (San Fernando to Princess Town)'],
                         ['North East (St Augustine to Arima)', 'North East (St Augustine to Arima)'],
                         ['East (Cumuto to Rio Claro)', 'East (Cumuto to Rio Claro)'],
                         ['Central (Caroni/Chaguanas/Couva to Claxton Bay)', 'Central (Caroni/Chaguanas/Couva to Claxton Bay)'],
                     ]" />
 
                 {{-- 17. I AM A NATIONAL OF TRINIDAD AND TOBAGO --}}
-                <x-form.radio id="APL_TT" label="I am a national of Trinidad and Tobago" :options="$yesNoOptions" />
+                <x-form.radio id="APL_TT" label="I am a national of Trinidad and Tobago." :options="$yesNoOptions" />
 
                 {{-- 18. I POSSESS TWO CSEC PASSES OR MORE --}}
-                <x-form.radio id="APL_2_CXC_Passes" label="I possess two CSEC passes or more" :options="$yesNoOptions" />
+                <x-form.radio id="APL_2_CXC_Passes" label="I possess two (2) CXC/CSEC passes or more." :options="$yesNoOptions" />
 
                 {{-- 19. I POSSESS A CERTIFICATE REFLECTING COMPETENCIES IN GERIATRIC CARE OR PROFESSIONAL HEALTHCARE --}}
                 <div x-data="{ APL_Geriatric_Certif: '{{ old('APL_Geriatric_Certif', '') }}' }">
-                    <x-form.radio id="APL_Geriatric_Certif" label="I possess a certificate reflecting competencies in Geriatric Care or Professional Healthcare" :options="$yesNoOptions" x-model="APL_Geriatric_Certif" />
+                    <x-form.radio id="APL_Geriatric_Certif" label="I possess a certificate reflecting competencies in Geriatric Care or Professional Healthcare." :options="$yesNoOptions" x-model="APL_Geriatric_Certif" />
 
                     <div x-show="APL_Geriatric_Certif == 'Y'" x-cloak class="mt-4">
                         <x-form.text-input id="APL_Graduate" label="From which institution did you receive your certification?" :required="false" :questionNumber="false" />
@@ -228,13 +228,13 @@
                 <x-form.section-h1>Programme Interest</x-form.section-h1>
 
                 {{-- 21. I AM AVAILABLE TO PROVIDE GERIATRIC CARE BETWEEN THE HOURS 8:00 AM - 4:00 PM MONDAYS TO FRIDAYS --}}
-                <x-form.radio id="APL_Available_Weekdays" label="I am available to provide geriatric care between the hours 8:00 AM - 4:00 PM Mondays to Fridays (except public holidays) if successful" :options="$yesNoOptions" />
+                <x-form.radio id="APL_Available_Weekdays" label="I am available to provide Geriatric Care between the hours 8:00 AM - 4:00 PM Mondays to Fridays (except Public Holidays), if successful." :options="$yesNoOptions" />
 
                 {{-- 22. PLEASE OUTLINE YOUR EXPERIENCE IN PROVIDING GERIATRIC CARE SERVICES --}}
-                <x-form.text-area id="APL_Experience" label="Please outline your experience in providing geriatric care services" :required="true" />
+                <x-form.text-area id="APL_Experience" label="Please outline your experience in providing Geriatric Care Services." :required="true" />
 
                 {{-- 23. MOTIVATION & EXPECTATIONS --}}
-                <x-form.text-area id="APL_Motivation_Expectations" label="Motivation & Expectations - Briefly describe why you are interested in joining the National Service GAPP Programme" :required="true" />
+                <x-form.text-area id="APL_Motivation_Expectations" label="Motivation & Expectations - Briefly describe why you are interested in joining the National Service GAPP." :required="true" />
 
                 <x-form.section-h1>Feedback</x-form.section-h1>
 
@@ -244,10 +244,10 @@
                         <x-slot name="col1">
                             <x-form.select 
                                 id="APL_Post_Training_Intent" 
-                                label="After completing the National Service GAPP 6 months Programme, do you intend to:" 
+                                label="After completing the 6 months National Service GAPP, do you intend to:" 
                                 :options="[
-                                    ['Seek employment in geriatric care', 'Seek employment'],
-                                    ['Continue studies in healthcare', 'Continue studies'],
+                                    ['Seek employment in Geriatric Care', 'Seek employment'],
+                                    ['Continue studies in Healthcare', 'Continue studies'],
                                     ['Start your own caregiving business', 'Start business'],
                                     ['Other', 'Other'],
                                 ]"
@@ -269,7 +269,7 @@
                 </div>
 
                 {{-- 25. CONSENT FOR LONG-TERM FOLLOW-UP --}}
-                <x-form.radio id="APL_Consent_Followup" label="I consent to be contacted by the Ministry of Sport and Youth Affairs' Monitoring & Evaluation Unit up to two (2) years after programme completion for tracer studies" :options="$yesNoOptions" />
+                <x-form.radio id="APL_Consent_Followup" label="I consent to be contacted by the Ministry of Sport and Youth Affairs' Monitoring & Evaluation Unit up to two (2) years after programme completion for tracer studies." :options="$yesNoOptions" />
 
                 {{-- 26. HOW DID YOU FIND OUT ABOUT THE PROGRAMME? --}}
                 <div x-data="{ APL_How_Found_Programme: '{{ old('APL_How_Found_Programme', '') }}' }">
@@ -305,10 +305,10 @@
 
 
                 {{-- 27. Would you like to subscribe to the ministry's mailing list --}}
-                <x-form.radio id="APL_Subscribe_Mailing" label="Would you like to subscribe to the ministry's mailing list for updates on upcoming projects and programmes?" :options="$yesNoOptions" />
+                <x-form.radio id="APL_Subscribe_Mailing" label="Would you like to subscribe to the Ministry's mailing list for updates on upcoming projects and programmes?" :options="$yesNoOptions" />
 
                 {{-- 28. I agree to have my photographs or images used by the MSYA --}}
-                <x-form.radio id="APL_Photo_Consent" label="I agree to have my photographs or images used by the MSYA for promotion of the National Service GAPP Programme" :options="$yesNoOptions" />
+                <x-form.radio id="APL_Photo_Consent" label="I agree to have my photographs or images used by the MSYA for promotion of the National Service GAPP." :options="$yesNoOptions" />
 
                 <x-form.section-h1>Recommender Information</x-form.section-h1>
 
@@ -341,22 +341,22 @@
                 {{-- Required Documents --}}
                 <x-form.file-input id="File_Birth_Certificate" label="Birth Certificate" />
 
-                <x-form.file-input id="File_National_ID" label="National Identification Card Or Trinidad And Passport" />
+                <x-form.file-input id="File_National_ID" label="National Identification Card or Trinidad and Tobago Passport" />
 
                 <x-form.file-input id="File_Proof_Address" label="Proof of Address (Utility Bill or Top of Bank Statement)" />
 
-                <x-form.file-input id="File_Authorization_Letter" label="Letter Of Authorization" :required="false" />
+                <x-form.file-input id="File_Authorization_Letter" label="Letter of Authorization" :required="false" />
 
-                <x-form.file-input id="File_Owner_ID" label="Owner’S Id" :required="false" />
+                <x-form.file-input id="File_Owner_ID" label="Owner’s ID" :required="false" />
                 
                 <x-form.file-input id="File_Geriatric_Certificate" label="Certificate in Geriatric Care/Professional Healthcare" :required="false"/>
 
-                <x-form.multi-file-input id="Files_Academic_Certificates" label="Upload Your Academic Certificates Here" :required="false"/>
+                <x-form.multi-file-input id="Files_Academic_Certificates" label="Upload Your Academic Certificates Here." :required="false"/>
                 
                 <div x-data="{ APL_Character_Selection: '{{ old('APL_Character_Selection', '') }}' }">
-                    <x-form.radio id="APL_Character_Selection" label="Please select either certificate of character or the receipt number to upload" :options="[
+                    <x-form.radio id="APL_Character_Selection" label="Please select either Certificate of Character or the Receipt Number to upload." :options="[
                         ['Certificate of Character', 'COC'],
-                        ['Certificate Of Character Receipt Number', 'CRN']
+                        ['Certificate of Character Receipt Number', 'CRN']
                     ]" x-model="APL_Character_Selection" />
                     
                     <div x-show="APL_Character_Selection === 'CRN'" x-cloak>
@@ -378,13 +378,13 @@
                 <x-form.file-input id="File_NIS_Card" label="National Insurance Card (Optional)" :required="false" />
 
                 <x-form.wrapper>
-                    <h3 class="fw-bold text-center">NOTE</h3>
+                    <h1 class="fw-bold text-center">NOTE</h1>
                     <hr>
                     <p class="text-justify">
-                        Participants must be willing to sign a participation agreement and work to meet the learning objectives and requirements of the training. This form and information collected within is confidential and intended for use by the Ministry of Sport and Youth Affairs. Your information will remain private and confidential and will not be used for other purposes other than the above mentioned.
+                        Participants must be willing to sign a Participation Agreement and work to meet the learning objectives and requirements of the training. This form and information collected within is Confidential and intended for use by the Ministry of Sport and Youth Affairs. Your information will remain Private and Confidential and will not be used for other purposes other than the above mentioned.
                     </p>
                     <p class="text-justify">
-                        I hereby declare that the information given in this application is true and correct to the best of my knowledge and belief. If any information given in this application proves to be false or incorrect, I accept the consequences of automatic rejection of the submission.
+                        I hereby declare that the information given in this application is true and correct to the best of my knowledge and belief. If any information given in this application proves to be false or incorrect, I accept the consequences of automatic rejection of the submission and I may be liable for any breach of the applicable laws of the Republic of Trinidad and Tobago.
                     </p>
                 </x-form.wrapper>
 
